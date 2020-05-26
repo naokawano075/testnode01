1f2fb379d806594b2376c6babd541dad1be59872
naokawano075 <tenpai0255sn@yahoo.co.jp>
2020-05-26 14:51:44 +0900

    test01

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+var server = http.createServer(
+    (request,response)=>{
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<DOCTYPE html><html lang="ja">');
+        response.write('<head><meta charset="utf-8">');
+        response.write('<title>Hello</title></head>');
+        response.write('<body><h1>Hello Node.js</h1>');
+        response.write('<p><This is Node.js sample page.></p>');
+        response.write('<p>これはNode.jsのサンプルページです。</p>', 'utf8');
+        response.write('</body></html>');
+        response.end();
+    }
+);
+server.listen(3000);
